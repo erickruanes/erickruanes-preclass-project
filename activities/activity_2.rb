4a3a7dc9020539cd20fa343492e66e0f7f294b1d
erickruanes <ruaneserick@gmail.com>
2022-06-17 17:32:36 +0800

    activity 2: Create a function that will show the full name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+
+def full_name(names)
+
+  puts "#{names[:First_name]}"
+  puts "#{names[:Middle_name]}"
+  puts "#{names[:Lastname]}"
+
+
+
+end
+p full_name({First_name: "Erick", Middle_name: "Paler", Lastname: "Ruanes"})
